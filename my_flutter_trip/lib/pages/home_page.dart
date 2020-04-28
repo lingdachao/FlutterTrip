ef956376401962a0714cfa0315050a7f84c07ac2
lingdachao <463569566@qq.com>
2020-04-28 16:16:10 +0800

    add swiper banner

--- my_flutter_trip/lib/pages/home_page.dart
+++ my_flutter_trip/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,11 +8,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List _imageUrls = [
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                  itemCount: _imageUrls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext context, int index){
+                      return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill);
+                  },
+                  pagination: SwiperPagination()),
+            )
+          ],
+        ),
       ),
     );
   }
